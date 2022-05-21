b0e1601236efecbc4de628cb4586c60b0d92a50e
King Lemming <kinglemming@gmail.com>
2022-05-20 22:37:39 -0700

    1.18.2 Port

--- src/main/java/cofh/thermal/integration/ThermalIntegration.java
+++ src/main/java/cofh/thermal/integration/ThermalIntegration.java
@@ -1,16 +1,13 @@
 package cofh.thermal.integration;
 
-import cofh.thermal.integration.init.TIntConfig;
 import net.minecraftforge.fml.common.Mod;
 
 import static cofh.lib.util.constants.Constants.ID_THERMAL_INTEGRATION;
 
-@Mod(ID_THERMAL_INTEGRATION)
+@Mod (ID_THERMAL_INTEGRATION)
 public class ThermalIntegration {
 
     public ThermalIntegration() {
-
-        TIntConfig.register();
 
         setFeatureFlags();
     }
