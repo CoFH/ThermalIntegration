2b67e78d2b4f6f1c925a357888c74caa675cbb81
King Lemming <kinglemming@gmail.com>
2022-05-23 22:32:58 -0700

    Good for initial release, I suppose.

--- src/main/java/cofh/thermal/integration/data/TIntRecipeProvider.java
+++ src/main/java/cofh/thermal/integration/data/TIntRecipeProvider.java
@@ -1,16 +1,16 @@
 package cofh.thermal.integration.data;
 
 import cofh.lib.data.RecipeProviderCoFH;
-import cofh.lib.util.references.CoFHTags;
 import cofh.thermal.lib.common.ThermalFlags;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
-import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 
 import java.util.function.Consumer;
 
 import static cofh.lib.util.constants.Constants.ID_THERMAL;
 import static cofh.thermal.core.ThermalCore.ITEMS;
+import static cofh.thermal.lib.common.ThermalFlags.FLAG_RESOURCE_ROSE_GOLD;
+import static cofh.thermal.lib.common.ThermalFlags.FLAG_RESOURCE_STEEL;
 
 public class TIntRecipeProvider extends RecipeProviderCoFH {
 
@@ -29,26 +29,17 @@ public class TIntRecipeProvider extends RecipeProviderCoFH {
     @Override
     protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
 
-        var reg = ITEMS;
-
-        generateTypeRecipes(reg, consumer, "steel");
-        generateTypeRecipes(reg, consumer, "rose_gold");
-
-        generateSmeltingAndBlastingRecipes(reg, consumer, "steel", 0);
-        generateSmeltingAndBlastingRecipes(reg, consumer, "rose_gold", 0);
+        generateAlloyRecipes("steel", withConditions(consumer).flag(FLAG_RESOURCE_STEEL));
+        generateAlloyRecipes("rose_gold", withConditions(consumer).flag(FLAG_RESOURCE_ROSE_GOLD));
     }
 
     // region HELPERS
-    private void generateAlloyRecipes(Consumer<FinishedRecipe> consumer) {
+    private void generateAlloyRecipes(String type, Consumer<FinishedRecipe> consumer) {
 
         var reg = ITEMS;
 
-        ShapelessRecipeBuilder.shapeless(reg.get("rose_gold_dust"), 2)
-                .requires(CoFHTags.Items.DUSTS_COPPER)
-                .requires(CoFHTags.Items.DUSTS_GOLD)
-                .unlockedBy("has_copper_dust", has(CoFHTags.Items.DUSTS_COPPER))
-                .unlockedBy("has_gold_dust", has(CoFHTags.Items.DUSTS_GOLD))
-                .save(consumer, ID_THERMAL + ":rose_gold_dust_2");
+        generateTypeRecipes(reg, consumer, type);
+        generateSmeltingAndBlastingRecipes(reg, consumer, type, 0);
     }
     // endregion
 }
